7cb0c33df3c15798ac47c1980196cd8fe4557cda
Krunal Mistry <krunalmistry@Krunals-MacBook-Air.local>
2020-12-30 13:01:31 -0500

    adding code

---
+++ Profile_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Profile_app
+//
+//  Created by Krunal Mistry on 12/30/20.
+//  Copyright © 2020 Apolis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
